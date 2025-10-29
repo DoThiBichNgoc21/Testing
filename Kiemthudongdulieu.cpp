75aed870140fbb2bdafef99715d9ecf5b9643a14
DoThiBichNgoc21 <161515353+DoThiBichNgoc21@users.noreply.github.com>
2025-10-29 18:02:16 +0700

    Create Kiemthudongdulieu.cpp
    ssss

---
+++ Kiemthudongdulieu.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int tinhPhanTramGiamGia(int t, int x, bool c) {
+    if (t < 3 || t > 120 || x < 2 || x > 8) {
+        return -1;
+    }
+    if ((t < 18 || t > 59) && c) {
+        return -1;
+    }
+    int giam = 0;
+    if (t < 12) {
+        giam = 30;
+    } else if (t >= 12 && t <= 17) {
+        giam = 20;
+    } else if (t >= 18 && t <= 59) {
+        if (c) {
+            giam = 20;
+        } else {
+            giam = 0;
+        }
+    } else {
+        giam = 30;
+    }
+    if (x == 7 || x == 8) {
+        giam += 10;
+    }
+    if (giam > 100) {
+        giam = 100;
+    }
+    return giam;
+}
+int main() {
+    int t, x;
+    bool c;
+    cin >> t >> x >> c;
+    int ketQua = tinhPhanTramGiamGia(t, x, c);
+    if (ketQua == -1) {
+        cout << "Dau vao khong hop le" << endl;
+    } else {
+        cout << ketQua << "%" << endl;
+    }
+    return 0;
+}
